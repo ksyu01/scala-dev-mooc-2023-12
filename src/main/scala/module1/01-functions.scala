6e3ced99b96cb9c9f39e6aec77acd50bcd1c345c
stanislav <ksyu01@yahoo.com>
2024-01-05 17:08:26 +0300

    Lesson1

--- src/main/scala/module1/01-functions.scala
+++ src/main/scala/module1/01-functions.scala
@@ -1,6 +1,6 @@
 package module1
 
-object functions {
+object functions extends App {
 
 
   /**
@@ -38,7 +38,7 @@ object functions {
 
   val divide2: PartialFunction[(Int, Int), Int] = {
     case x if x._2 != 0 => x._1 / x._2
-    case x if x._2 != 1 => x._1 / x._2
+//    case x if x._2 != 1 => x._1 / x._2
   }
 
   val l = List((4, 2), (5, 0), (6, 3)).collect(divide2)
@@ -52,28 +52,57 @@ object functions {
   /**
    *  Задание 1. Написать ф-цию метод isEven, которая будет вычислять является ли число четным
    */
-
+  def isEven(x: Int): Boolean = {
+    x % 2 == 0
+  }
 
   /**
    * Задание 2. Написать ф-цию метод isOdd, которая будет вычислять является ли число нечетным
    */
-
+  def isOdd(x: Int): Boolean = {
+    x % 2 != 0
+  }
 
   /**
    * Задание 3. Написать ф-цию метод filterEven, которая получает на вход массив чисел и возвращает массив тех из них,
    * которые являются четными
    */
-
+  def filterEven(numbers: Array[Int]): Array[Int] = {
+    numbers.filter(x => isEven(x))
+  }
 
 
   /**
    * Задание 4. Написать ф-цию метод filterOdd, которая получает на вход массив чисел и возвращает массив тех из них,
    * которые являются нечетными
    */
-
+  def filterOdd(numbers: Array[Int]): Array[Int] = {
+    numbers.filter(isOdd)
+  }
 
   /**
    * return statement
    *
    */
+  val myRange: Range = 0 to 3
+
+  // isEven
+  myRange.foreach(x => println(s"isEven($x): " + isEven(x)))
+
+  // isOdd
+  myRange.foreach(x => println(s"isOdd($x): " + isOdd(x)))
+
+  val myArray = myRange.toArray
+  // filterEven
+  println{
+    s"filterEven(${myArray.mkString(", ")}) => " +
+      filterEven(myArray).mkString(", ")
+  }
+
+  // filterOdd
+  println{
+    s"filterOdd(${myArray.mkString(", ")}) => " +
+      filterOdd(myArray).mkString(", ")
+  }
+
 }
