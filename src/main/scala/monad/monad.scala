c92da7d43eaf70c1a97e1292f50bcba3db1a01f6
stanislav <ksyu01@yahoo.com>
2024-02-10 17:16:49 +0300

    Homework3 Monad

--- src/main/scala/monad/monad.scala
+++ src/main/scala/monad/monad.scala
@@ -5,29 +5,31 @@ package object monad {
    * HINT: для проверки на пустой элемент можно использовать eq
    */
 
-  trait Wrap[+A] {
+  sealed trait Wrap[+A] {
 
     def get: A
 
-    def pure[R](x: R): Wrap[R] = ???
-
-    def flatMap[R](f: A => Wrap[R]): Wrap[R] = {
-      ???
+    def flatMap[R](f: A => Wrap[R]): Wrap[R] = this match {
+      case NonEmptyWrap(result) => f(result)
+      case EmptyWrap => EmptyWrap
     }
 
     // HINT: map можно реализовать через pure и flatMap
-    def map[R](f: A => R): Wrap[R] = {
-      ???
-    }
+    def map[R](f: A => R): Wrap[R] = flatMap(t => Wrap.pure(f(t)))
 
-    def withFilter(f: A => Boolean): Wrap[A] = {
-      ???
+    def withFilter(f: A => Boolean): Wrap[A] = this match {
+      case NonEmptyWrap(result) => if (f(result)) this else EmptyWrap
+      case EmptyWrap => EmptyWrap
     }
 
   }
 
   object Wrap {
     def empty[R]: Wrap[R] = EmptyWrap
+
+    def pure[R](x: R): Wrap[R] =
+      if (x == null) EmptyWrap
+      else NonEmptyWrap(x)
   }
 
   case class NonEmptyWrap[A](result: A) extends Wrap[A] {
